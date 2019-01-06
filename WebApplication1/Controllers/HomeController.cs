9b3ec7e856faadc52841999af75e4fd536fd2413
Bochmashina <boiko.tsvetanov@abv.bg>
2019-01-06 15:36:45 +0200

    First Commit PISS project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        JokesEntities db = new JokesEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult btnRandomJoke_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int randomJokeId = rand.Next(1, db.Jokes.Count() + 1);//from 1 to size of Jokes database
+            string message = "Не съществуват вицове в базата данни.";
+            foreach(var item in db.Jokes)
+            {
+                if (item.Id == randomJokeId)
+                {
+                    message = "<br>" + item.Content;
+                    break;
+                }
+            }
+            return new JsonResult { Data = message, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public ActionResult GetJokesByCategory(string category)
+        {
+            string jokes = "";
+            foreach (var item in db.Jokes)
+            {
+                if (item.Categories.Contains(category))
+                {
+                    jokes += "<br>" + item.Content + "<br>";
+                }
+            }
+            if(jokes=="")
+            {
+                jokes = "Не съществуват вицове от тази категория.";
+            }
+            return new JsonResult { Data = jokes, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        
+        public void AddJokeToDatabase(string content, string categories)
+        {
+            Jokes joke = new Jokes();
+            joke.Content = content;
+            joke.Categories = categories;
+            db.Jokes.Add(joke);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/JokesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class JokesController : Controller
+    {
+        JokesEntities db = new JokesEntities();
+        // GET: Jokes
+        public ActionResult Index()
+        {
+            var jokes = from j in db.Jokes
+                        select j;
+            return View(jokes.ToList());
+        }
+    }
+}
